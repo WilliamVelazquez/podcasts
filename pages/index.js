1bddcf0f2a402a845bf283974e7353b9ede2f372
William Velazquez <william.velazquez.a@gmail.com>
2018-07-14 00:02:05 -0500

    getInitialProps
    Load HomePage Content with an API
    Importing isomorphic-fetch

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,66 @@
+import 'isomorphic-fetch'
+
 export default class extends React.Component {
+
+  static async getInitialProps(){
+    let req = await fetch('https://api.audioboom.com/channels/recommended')
+    let { body: channels } = await req.json()
+    return { channels }
+  }
+
   render() {
+    const { channels } = this.props
+
     return <div>
-      <h1>¡Hola mundo!</h1>
-      <p>Bienvenido al curso</p>
+      <header>Podcasts</header>
 
-      <img src="/static/platzi-logo.png" alt="Platzi"/>
-
+      <div className="channels">
+        {
+          channels.map((channel) => (
+            <div className="channel">
+              <img src={channel.urls.logo_image.original} alt={channel.title}/>
+              <h2>{channel.title}</h2>
+            </div>
+          ))
+        }
+      </div>
       <style jsx>{`
-        h1{
-          color: red;
+        header{
+          color: #fff;
+          background: #8756ca;
+          padding: 15px;
+          text-align: center;
         }
-        p{
-          color: green;
+        .channels {
+          display: grid;
+          grid-gap: 15px;
+          padding: 15px;
+          grid-template-columns: repeat(auto-fill, minmax(160px, 1fr));
         }
-        img{
-          max-width: 50%;
+        a.channel {
           display: block;
-          margin: 0 auto;
+          margin-bottom: 0.5em;
+          color: #333;
+          text-decoration: none;
+        }
+        .channel img {
+          border-radius: 3px;
+          box-shadow: 0px 2px 6px rgba(0,0,0,0.15);
+          width: 100%;
+        }
+        h2 {
+          padding: 5px;
+          font-size: 0.9em;
+          font-weight: 600;
+          margin: 0;
+          text-align: center;
         }
       `}</style>
 
       <style jsx global>{`
         body{
+          margin: 0;
+          font-family: system-ui;
           background: white;
         }
       `}</style>
