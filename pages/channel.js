08c21538750c7514587d30a47edf93001b0e8e80
William Velazquez <william.velazquez.a@gmail.com>
2018-07-14 15:16:46 -0500

    React Components to Structure the Index Page
    Creating React Components
    Adding Link to the HomePage from the App Logo

--- pages/channel.js
+++ pages/channel.js
@@ -1,5 +1,7 @@
 import Link from 'next/link'
 
+import Layout from '../components/Layout'
+
 export default class extends React.Component{
 
   static async getInitialProps({ query }){
@@ -27,8 +29,7 @@ export default class extends React.Component{
   render() {
     const { channel, audioClips, series } = this.props
     return (
-      <div>
-        <header>Podcasts</header>
+      <Layout>
         <div className="banner" style={{ backgroundImage: `url(${channel.urls.banner_image.original})` }} />
         
         <h1>{ channel.title }</h1>
@@ -68,77 +69,77 @@ export default class extends React.Component{
         
 
         <style jsx>{`
-        header{
-          color: #fff;
-          background: #8756ca;
-          padding: 15px;
-          text-align: center;
-        }
+          header{
+            color: #fff;
+            background: #8756ca;
+            padding: 15px;
+            text-align: center;
+          }
 
-        .banner {
-          width: 100%;
-          padding-bottom: 25%;
-          background-position: 50% 50%;
-          background-size: cover;
-          background-color: #aaa;
-        }
+          .banner {
+            width: 100%;
+            padding-bottom: 25%;
+            background-position: 50% 50%;
+            background-size: cover;
+            background-color: #aaa;
+          }
 
-        .channels {
-          display: grid;
-          grid-gap: 15px;
-          padding: 15px;
-          grid-template-columns: repeat(auto-fill, minmax(160px, 1fr));
-        }
+          .channels {
+            display: grid;
+            grid-gap: 15px;
+            padding: 15px;
+            grid-template-columns: repeat(auto-fill, minmax(160px, 1fr));
+          }
 
-        a.channel {
-          display: block;
-          margin-bottom: 0.5em;
-          color: #333;
-          text-decoration: none;
-        }
+          a.channel {
+            display: block;
+            margin-bottom: 0.5em;
+            color: #333;
+            text-decoration: none;
+          }
 
-        .channel img {
-          border-radius: 3px;
-          box-shadow: 0px 2px 6px rgba(0,0,0,0.15);
-          width: 100%;
-        }
+          .channel img {
+            border-radius: 3px;
+            box-shadow: 0px 2px 6px rgba(0,0,0,0.15);
+            width: 100%;
+          }
 
-        h1{
-          font-weight: 600;
-          padding: 15px;
-        }
+          h1{
+            font-weight: 600;
+            padding: 15px;
+          }
 
-        h2 {
-          padding: 5px;
-          font-size: 0.9em;
-          font-weight: 600;
-          margin: 0;
-          text-align: center;
-        }
+          h2 {
+            padding: 5px;
+            font-size: 0.9em;
+            font-weight: 600;
+            margin: 0;
+            text-align: center;
+          }
 
-        .podcast {
-          display: block;
-          text-decoration: none;
-          color: #333;
-          padding: 15px;
-          border-bottom: 1px solid rgba(0,0,0,0.2);
-          cursor: pointer;
-        }
+          .podcast {
+            display: block;
+            text-decoration: none;
+            color: #333;
+            padding: 15px;
+            border-bottom: 1px solid rgba(0,0,0,0.2);
+            cursor: pointer;
+          }
 
-        .podcast:hover {
-          color: #000;
-        }
+          .podcast:hover {
+            color: #000;
+          }
 
-        .podcast h3 {
-          margin: 0;
-        }
+          .podcast h3 {
+            margin: 0;
+          }
 
-        .podcast .meta {
-          color: #666;
-          margin-top: 0.5em;
-          font-size: 0.8em;
-        }
-      `}</style>
+          .podcast .meta {
+            color: #666;
+            margin-top: 0.5em;
+            font-size: 0.8em;
+          }
+        `}</style>
 
         <style jsx global>{`
         body{
@@ -146,8 +147,9 @@ export default class extends React.Component{
           font-family: system-ui;
           background: white;
         }
-      `}</style>
-      </div>
+        `}</style>
+        
+      </Layout>
     );
   }
 }
--- pages/podcast.js
+++ pages/podcast.js
@@ -1,5 +1,7 @@
 import Link from 'next/link'
 
+import Layout from '../components/Layout'
+
 export default class extends React.Component{
 
   static async getInitialProps({ query }){
@@ -14,8 +16,7 @@ export default class extends React.Component{
   render() {
     const { clip } = this.props
     return (
-      <div>
-        <header>Podcasts</header>
+      <Layout>
 
         <div className='modal'>
           <div className='clip'>
@@ -40,75 +41,75 @@ export default class extends React.Component{
         </div>
 
         <style jsx>{`
-        nav {
-          background: none;
-        }
-        nav a {
-          display: inline-block;
-          padding: 15px;
-          color: white;
-          cursor: pointer;
-          text-decoration: none;
-        }
-        .clip {
-          display: flex;
-          height: 100%;
-          flex-direction: column;
-          background: #8756ca;
-          color: white;
-        }
-        picture {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex: 1 1;
-          flex-direction: column;
-          width: auto;
-          padding: 10%;
-        }
-        picture div {
-          width: 100%;
-          height: 100%;
-          background-position: 50% 50%;
-          background-size: contain;
-          background-repeat: no-repeat;
-        }
-        .player {
-          padding: 30px;
-          background: rgba(0,0,0,0.3);
-          text-align: center;
-        }
-        h3 {
-          margin: 0;
-        }
-        h6 {
-          margin: 0;
-          margin-top: 1em;
-        }
-        audio {
-          margin-top: 2em;
-          width: 100%;
-        }
+          nav {
+            background: none;
+          }
+          nav a {
+            display: inline-block;
+            padding: 15px;
+            color: white;
+            cursor: pointer;
+            text-decoration: none;
+          }
+          .clip {
+            display: flex;
+            height: 100%;
+            flex-direction: column;
+            background: #8756ca;
+            color: white;
+          }
+          picture {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            flex: 1 1;
+            flex-direction: column;
+            width: auto;
+            padding: 10%;
+          }
+          picture div {
+            width: 100%;
+            height: 100%;
+            background-position: 50% 50%;
+            background-size: contain;
+            background-repeat: no-repeat;
+          }
+          .player {
+            padding: 30px;
+            background: rgba(0,0,0,0.3);
+            text-align: center;
+          }
+          h3 {
+            margin: 0;
+          }
+          h6 {
+            margin: 0;
+            margin-top: 1em;
+          }
+          audio {
+            margin-top: 2em;
+            width: 100%;
+          }
 
-        .modal {
-          position: fixed;
-          top: 0;
-          left: 0;
-          right: 0;
-          bottom: 0;
-          z-index: 99999;
-        }
-      `}</style>
+          .modal {
+            position: fixed;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            z-index: 99999;
+          }
+        `}</style>
 
-      <style jsx global>{`
-        body {
-          margin: 0;
-          font-family: system-ui;
-          background: white;
-        }
-      `}</style>
-
-      </div>
+        <style jsx global>{`
+          body {
+            margin: 0;
+            font-family: system-ui;
+            background: white;
+          }
+        `}</style>
+        
+      </Layout>
     );
   }
 }
